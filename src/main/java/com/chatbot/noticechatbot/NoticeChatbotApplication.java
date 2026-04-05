1e00c4699d54c277abbb417a18575c7404ff3ee9
jominho37 <jominho37@gmail.com>
2026-04-05 13:42:15 +0900

    :tada: Start : Start NoticeChatbot Project

---
+++ src/main/java/com/chatbot/noticechatbot/NoticeChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.chatbot.noticechatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoticeChatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoticeChatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chatbot/noticechatbot/NoticeChatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatbot.noticechatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoticeChatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
